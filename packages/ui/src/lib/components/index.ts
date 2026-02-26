ea1a395412e1063f95c30955b13e60441519032a
JRSalica <salicajorge@gmail.com>
2026-02-25 23:17:45 -0300

    feat: Add index files to export `InfiniteScroll`, `Navbar`, `FlowLayout` components and the `Landing` page.

--- packages/ui/src/lib/components/index.ts
+++ packages/ui/src/lib/components/index.ts
@@ -1,6 +1,5 @@
 // Common shared components
 export { default as InfiniteScroll } from './common/InfiniteScroll.svelte';
-export { default as MetricCard } from './common/MetricCard.svelte';
 
 // Layout components
 export { default as Navbar } from './layout/Navbar.svelte';
--- packages/ui/src/lib/pages/index.ts
+++ packages/ui/src/lib/pages/index.ts
@@ -1,4 +1,1 @@
 export { default as Landing } from './Landing.svelte';
-export { default as SpotifyFlow } from '../flows/spotify/SpotifyFlow.svelte';
-export { default as LyricsFlow } from '../flows/lyrics/LyricsFlow.svelte';
-export { default as TradingFlow } from '../flows/trading/TradingFlow.svelte';
